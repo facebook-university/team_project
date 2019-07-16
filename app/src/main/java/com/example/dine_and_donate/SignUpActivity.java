3c5627c256c399070364d6242d857d5358d4a1f4
shry678 <shry678@gmail.com>
2019-07-16 14:45:15 -0700

    finalized login/signup pages

--- app/src/main/java/com/example/dine_and_donate/SignUpActivity.java
+++ app/src/main/java/com/example/dine_and_donate/SignUpActivity.java
@@ -1,9 +1,11 @@
 package com.example.dine_and_donate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -16,6 +18,8 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
     private EditText email;
     private EditText password;
     private EditText orgPhone;
+    private Button signUpBtn;
+    private Button backToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +31,16 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         email = findViewById(R.id.et_email);
         password = findViewById(R.id.et_password2);
         orgPhone = findViewById(R.id.et_org_phone);
+        signUpBtn = findViewById(R.id.final_signup_btn);
+        backToLogin = findViewById(R.id.back_to_login_btn);
 
         name.setVisibility(View.GONE);
         email.setVisibility(View.GONE);
         password.setVisibility(View.GONE);
         orgPhone.setVisibility(View.GONE);
-
+        signUpBtn.setVisibility(View.GONE);
 
         //display specific text views depending on user type selected
-        //userType.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
         spinner = findViewById(R.id.user_options);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_types, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -45,26 +50,59 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
         spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItem = parent.getSelectedItem().toString();
+                //show all fields except phone number for consumer user type
                 if (selectedItem.equals("Consumer")) {
                     name.setVisibility(View.VISIBLE);
                     email.setVisibility(View.VISIBLE);
                     password.setVisibility(View.VISIBLE);
+                    signUpBtn.setVisibility(View.VISIBLE);
                     orgPhone.setVisibility(View.GONE);
                 }  else if(spinner.getSelectedItem().toString().equals("Organization")) {
                     orgPhone.setVisibility(View.VISIBLE);
                     name.setVisibility(View.VISIBLE);
                     email.setVisibility(View.VISIBLE);
+                    signUpBtn.setVisibility(View.VISIBLE);
                     password.setVisibility(View.VISIBLE);
                 } else {
                     name.setVisibility(View.GONE);
                     email.setVisibility(View.GONE);
                     password.setVisibility(View.GONE);
                     orgPhone.setVisibility(View.GONE);
+                    signUpBtn.setVisibility(View.GONE);
                 }
             }
 
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        //go to main page (map) once signed up
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean validInfo = !(emptyField(name) && emptyField(email) && emptyField(password) && emptyField(orgPhone));
+                //if none of the fields are empty, then sign up information is valid
+                if(validInfo) {
+                    //if password length is less than 5, then user can not successfully sign up
+                    if(password.getText().toString().length() < 5) {
+                        password.setError("You must have at least 5 characters");
+                    } else {
+                        Intent intent = new Intent(SignUpActivity.this, MapActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            }
+        });
+
+        //go back to login page
+        backToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
@@ -74,15 +112,16 @@ public class SignUpActivity extends AppCompatActivity implements AdapterView.OnI
 
     }
 
-//    //method to check that the fields are not empty
-//    private boolean checkIfEmpty(String userInput) {
-//        String userEmail = userInput.getText().toString().trim();
-//        if(userEmail.isEmpty()) {
-//            emailInput.setError("Field can not be empty");
-//            return false;
-//        } else {
-//            emailInput.setError(null);
-//            return true;
-//        }
-//    }
+    //method to check if fields are empty
+    private boolean emptyField(EditText text) {
+        String userEmail = text.getText().toString().trim();
+        //return true if field is empty
+        if(userEmail.isEmpty()) {
+            text.setError("Field can not be empty");
+            return true;
+        } else {
+            text.setError(null);
+            return false;
+        }
+    }
 }
