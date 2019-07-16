2f7b644030d99752b46255f01a2f5bb998556059
shry678 <shry678@gmail.com>
2019-07-16 10:47:11 -0700

    created activities for login and signup pages

--- app/src/main/java/com/example/dine_and_donate/SignUpActivity.java
+++ app/src/main/java/com/example/dine_and_donate/SignUpActivity.java
@@ -1,4 +1,75 @@
 package com.example.dine_and_donate;
 
-public class SignUpActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private Spinner spinner;
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private EditText orgPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+
+        spinner = findViewById(R.id.user_options);
+        name = findViewById(R.id.et_name);
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password2);
+        orgPhone = findViewById(R.id.et_org_phone);
+
+        name.setVisibility(View.GONE);
+        email.setVisibility(View.GONE);
+        password.setVisibility(View.GONE);
+        orgPhone.setVisibility(View.GONE);
+
+
+        //display specific text views depending on user type selected
+        //userType.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
+        spinner = findViewById(R.id.user_options);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        //show appropriate text views based on user type selection
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getSelectedItem().toString();
+                if (selectedItem.equals("Consumer")) {
+                    name.setVisibility(View.VISIBLE);
+                    email.setVisibility(View.VISIBLE);
+                    password.setVisibility(View.VISIBLE);
+                    orgPhone.setVisibility(View.GONE);
+                }  else if(spinner.getSelectedItem().toString().equals("Organization")) {
+                    orgPhone.setVisibility(View.VISIBLE);
+                    name.setVisibility(View.VISIBLE);
+                    email.setVisibility(View.VISIBLE);
+                    password.setVisibility(View.VISIBLE);
+                } else {
+                    name.setVisibility(View.GONE);
+                    email.setVisibility(View.GONE);
+                    password.setVisibility(View.GONE);
+                    orgPhone.setVisibility(View.GONE);
+                }
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
 }
