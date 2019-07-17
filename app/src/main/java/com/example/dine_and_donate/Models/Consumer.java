f03ec1a652867132aa5105992d4874be8ac51313
Ryan J Beck <ryanbeck@fb.com>
2019-07-17 14:10:45 -0700

    user objects created in db when new account is made

--- app/src/main/java/com/example/dine_and_donate/Models/Consumer.java
+++ app/src/main/java/com/example/dine_and_donate/Models/Consumer.java
@@ -5,9 +5,14 @@ import java.util.ArrayList;
 public class Consumer {
 
     private String email;
+    private String name;
     private ArrayList<Event> upcomingEvents;
     private ArrayList<Event> pastEvents;
     private ArrayList<Organization> followingOrgs;
 
 
+    public Consumer(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
 }
