9d17ab288569e6fd771e681adfad9bbbf78821ba
shry678 <shry678@gmail.com>
2019-07-26 14:59:01 -0700

    resolved merge conflicts

--- app/src/main/java/com/example/dine_and_donate/Activities/HomeActivity.java
+++ app/src/main/java/com/example/dine_and_donate/Activities/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.dine_and_donate.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
