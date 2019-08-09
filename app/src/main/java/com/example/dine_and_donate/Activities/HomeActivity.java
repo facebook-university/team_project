bfa6e33db7e8aa8f8ae3aa53bc1698088f6bbdbd
Ryan J Beck <ryanbeck@fb.com>
2019-08-09 12:52:18 -0700

    saved events now show up right away in profile

--- app/src/main/java/com/example/dine_and_donate/Activities/HomeActivity.java
+++ app/src/main/java/com/example/dine_and_donate/Activities/HomeActivity.java
@@ -64,6 +64,7 @@ public class HomeActivity extends AppCompatActivity {
     private String mStack = "map";
     private String mClickedOnID;
     private Boolean mIsOnProfileView;
+    private boolean mNewSavedEvent = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -285,6 +286,14 @@ public class HomeActivity extends AppCompatActivity {
 
     public void setClickedOnIdToNull() {
         mClickedOnID = null;
+    }
+
+    public boolean isNewSavedEvent() {
+        return mNewSavedEvent;
+    }
+
+    public void setNewSavedEvent(boolean mNewSavedEvent) {
+        this.mNewSavedEvent = mNewSavedEvent;
     }
 
     public static class MyReceiver extends BroadcastReceiver {
